99667643474d3ff7c969026b455fc1df6cfa9208
MelloMax <Tim985152482@outlook.com>
2021-03-29 17:26:41 +0800

    配置eslint+prettier+editor

--- vue.config.js
+++ vue.config.js
@@ -5,38 +5,35 @@ const VueAutoRoutingPlugin = require('vue-auto-routing/lib/webpack-plugin')
 const spritesmithPlugin = require('webpack-spritesmith')
 
 module.exports = {
-  // ... other options ...
-  publicPath: './',
-  chainWebpack: config => {
-
-  },
-  configureWebpack: {
-    target: 'web',
-    plugins: [
-      new VueAutoRoutingPlugin({
-        // Path to the directory that contains your page components.
-        pages: 'src/views',
-
-        // A string that will be added to importing component path (default @/pages/).
-        importPrefix: '@/views/'
-      }), // 路由自动化
-      // eslint-disable-next-line new-cap
-      new spritesmithPlugin({
-        src: {
-          cwd: resolve('src/assets/images'),
-          glob: '*.png'
-        },
-        target: {
-          image: resolve('src/spritesmith-generated/sprite.png'),
-          css: resolve('src/spritesmith-generated/sprite.scss')
-        },
-        apiOptions: {
-          cssImageRef: '~sprite.png'
-        },
-        spritesmithOptions: {
-          padding: 20
-        }
-      })
-    ]
-  }
+	// ... other options ...
+	publicPath: './',
+	chainWebpack: config => {},
+	configureWebpack: {
+		target: 'web',
+		plugins: [
+			new VueAutoRoutingPlugin({
+				// Path to the directory that contains your page components.
+				pages: 'src/views',
+				// A string that will be added to importing component path (default @/pages/).
+				importPrefix: '@/views/',
+			}), // 路由自动化
+			// eslint-disable-next-line new-cap
+			new spritesmithPlugin({
+				src: {
+					cwd: resolve('src/assets/images'),
+					glob: '*.png',
+				},
+				target: {
+					image: resolve('src/spritesmith-generated/sprite.png'),
+					css: resolve('src/spritesmith-generated/sprite.scss'),
+				},
+				apiOptions: {
+					cssImageRef: '~sprite.png',
+				},
+				spritesmithOptions: {
+					padding: 20,
+				},
+			}),
+		],
+	},
 }
